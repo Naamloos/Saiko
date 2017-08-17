8c1e803f431b94ef76b05af3cac5c42dac606617
NaamloosDT <ryandejonge@outlook.com>
2017-08-17 03:24:45 +0200

    netfx 4.6.2, and eval

--- Saiko/Saiko/Commands/Owner.cs
+++ Saiko/Saiko/Commands/Owner.cs
@@ -9,6 +9,10 @@ using DSharpPlus.Interactivity;
 using DSharpPlus.CommandsNext.Attributes;
 using System.Net;
 using System.IO;
+using Microsoft.CodeAnalysis.Scripting;
+using System.Diagnostics;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
 
 namespace Saiko.Commands
 {
@@ -57,6 +61,118 @@ namespace Saiko.Commands
                 }
             }
             await ctx.RespondAsync(left);
+        }
+
+        // stole- borrowed from emzi
+        [Command("eval"), Description("Evaluates a snippet of C# code, in context."), RequireOwner]
+        public async Task EvaluateAsync(CommandContext ctx, [RemainingText, Description("Code to evaluate.")] string code)
+        {
+            var cs1 = code.IndexOf("```") + 3;
+            cs1 = code.IndexOf('\n', cs1) + 1;
+            var cs2 = code.LastIndexOf("```");
+
+            if (cs1 == -1 || cs2 == -1)
+                throw new ArgumentException("You need to wrap the code into a code block.");
+
+            code = code.Substring(cs1, cs2 - cs1);
+
+            var embed = new DiscordEmbedBuilder
+            {
+                Title = "Evaluating...",
+                Color = Program.SaikoBot.Color
+            };
+            var msg = await ctx.RespondAsync("", embed: embed.Build()).ConfigureAwait(false);
+
+            var globals = new EvaluationEnvironment(ctx);
+            var sopts = ScriptOptions.Default
+                .WithImports("System", "System.Collections.Generic", "System.Linq", "System.Net.Http", "System.Net.Http.Headers", "System.Reflection", "System.Text", "System.Threading.Tasks",
+                    "DSharpPlus", "DSharpPlus.CommandsNext", "DSharpPlus.Interactivity", "SaikoPie.Helpers")
+                .WithReferences(AppDomain.CurrentDomain.GetAssemblies().Where(xa => !xa.IsDynamic && !string.IsNullOrWhiteSpace(xa.Location)));
+
+            var sw1 = Stopwatch.StartNew();
+            var cs = CSharpScript.Create(code, sopts, typeof(EvaluationEnvironment));
+            var csc = cs.Compile();
+            sw1.Stop();
+
+            if (csc.Any(xd => xd.Severity == DiagnosticSeverity.Error))
+            {
+                embed = new DiscordEmbedBuilder
+                {
+                    Title = "Compilation failed",
+                    Description = string.Concat("Compilation failed after ", sw1.ElapsedMilliseconds.ToString("#,##0"), "ms with ", csc.Length.ToString("#,##0"), " errors."),
+                    Color = Program.SaikoBot.Color
+                };
+                foreach (var xd in csc.Take(3))
+                {
+                    var ls = xd.Location.GetLineSpan();
+                    embed.AddField(string.Concat("Error at ", ls.StartLinePosition.Line.ToString("#,##0"), ", ", ls.StartLinePosition.Character.ToString("#,##0")), Formatter.InlineCode(xd.GetMessage()), false);
+                }
+                if (csc.Length > 3)
+                {
+                    embed.AddField("Some errors ommited", string.Concat((csc.Length - 3).ToString("#,##0"), " more errors not displayed"), false);
+                }
+                await msg.EditAsync(embed: embed.Build()).ConfigureAwait(false);
+                return;
+            }
+
+            Exception rex = null;
+            ScriptState<object> css = null;
+            var sw2 = Stopwatch.StartNew();
+            try
+            {
+                css = await cs.RunAsync(globals).ConfigureAwait(false);
+                rex = css.Exception;
+            }
+            catch (Exception ex)
+            {
+                rex = ex;
+            }
+            sw2.Stop();
+
+            if (rex != null)
+            {
+                embed = new DiscordEmbedBuilder
+                {
+                    Title = "Execution failed",
+                    Description = string.Concat("Execution failed after ", sw2.ElapsedMilliseconds.ToString("#,##0"), "ms with `", rex.GetType(), ": ", rex.Message, "`."),
+                    Color = Program.SaikoBot.Color,
+                };
+                await msg.EditAsync(embed: embed.Build()).ConfigureAwait(false);
+                return;
+            }
+
+            // execution succeeded
+            embed = new DiscordEmbedBuilder
+            {
+                Title = "Evaluation successful",
+                Color = Program.SaikoBot.Color,
+            };
+
+            embed.AddField("Result", css.ReturnValue != null ? css.ReturnValue.ToString() : "No value returned", false)
+                .AddField("Compilation time", string.Concat(sw1.ElapsedMilliseconds.ToString("#,##0"), "ms"), true)
+                .AddField("Execution time", string.Concat(sw2.ElapsedMilliseconds.ToString("#,##0"), "ms"), true);
+
+            if (css.ReturnValue != null)
+                embed.AddField("Return type", css.ReturnValue.GetType().ToString(), true);
+
+            await msg.EditAsync(embed: embed.Build()).ConfigureAwait(false);
+        }
+    }
+
+    public sealed class EvaluationEnvironment
+    {
+        public CommandContext Context { get; }
+
+        public DiscordMessage Message { get { return this.Context.Message; } }
+        public DiscordChannel Channel { get { return this.Context.Channel; } }
+        public DiscordGuild Guild { get { return this.Context.Guild; } }
+        public DiscordUser User { get { return this.Context.User; } }
+        public DiscordMember Member { get { return this.Context.Member; } }
+        public DiscordClient Client { get { return this.Context.Client; } }
+
+        public EvaluationEnvironment(CommandContext ctx)
+        {
+            this.Context = ctx;
         }
     }
 }
