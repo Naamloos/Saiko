3dac15d54f81cd14786b8cc07c36ec8d036edd2f
Ryan de Jonge <ryandejonge@outlook.com>
2024-02-04 23:22:28 +0100

    new project

---
+++ Saiko/Commands/Main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.Commands;
+using DSharpPlus.Commands.Processors.SlashCommands;
+using DSharpPlus.Commands.Trees.Attributes;
+using DSharpPlus.Entities;
+
+namespace Saiko.Commands
+{
+    public class Main
+    {
+        [Command("about"), Description("about Saiko")]
+        public static ValueTask AboutAsync(SlashCommandContext ctx)
+        {
+            return ctx.RespondAsync(new DiscordMessageBuilder()
+                .WithContent("Skibidi Toilet?"));
+        }
+    }
+}
--- Saiko/Program.cs
+++ Saiko/Program.cs
@@ -1,12 +1,61 @@
-﻿using System;
+﻿using DSharpPlus;
+using DSharpPlus.Commands;
+using DSharpPlus.Commands.Processors.SlashCommands;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Saiko
 {
     class Program
     {
-        static void Main(string[] args)
+        static DiscordClient client;
+
+        static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            if (!File.Exists("token.txt"))
+            {
+                Console.WriteLine("No token file found! File created (token.txt), please fill out!");
+                File.Create("token.txt");
+                Console.ReadKey();
+                return;
+            }
+
+            var token = "";
+            using (StreamReader sw = new StreamReader(File.OpenRead("token.txt")))
+            {
+                token = sw.ReadToEnd();
+            }
+
+            client = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = File.ReadAllText("token.txt").Trim(),
+                Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents
+            });
+
+            var collection = new ServiceCollection()
+                .AddSingleton(client);
+
+            var commands = client.UseCommands(new CommandsConfiguration()
+            {
+                ServiceProvider = collection.BuildServiceProvider()
+            });
+
+            commands.AddCommands(typeof(Program).Assembly);
+
+            await commands.AddProcessorAsync(new SlashCommandProcessor());
+
+            try
+            {
+                await client.ConnectAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            await Task.Delay(-1);
         }
     }
 }
